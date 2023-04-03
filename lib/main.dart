637d8afb1d3f036f30f4ceea75d8efb2d0b193e5
lucaslopes <lucas.lopesx1@gmail.com>
2023-04-02 21:33:49 -0300

    feat: imc calc page with routes navigation with params

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 
 void main() => runApp(const MyApp());
 
 const imcCalcRoute = 'imc-calc';
+RegExp decimalRegex = RegExp(r'^\d+\.?\d{0,2}');
 
 final GoRouter _router = GoRouter(
   routes: <RouteBase>[
@@ -14,12 +16,15 @@ final GoRouter _router = GoRouter(
       },
       routes: <RouteBase>[
         GoRoute(
-            name: imcCalcRoute,
-            path: '$imcCalcRoute/:weight/:height/:age/:sex',
-            builder: (BuildContext context, GoRouterState state) =>
-                ImcCalcScreen(
-                  weight: double.parse(state.params['weight']),
-                )),
+          name: imcCalcRoute,
+          path: '$imcCalcRoute/:weight/:height/:age/:sex',
+          builder: (BuildContext context, GoRouterState state) => ImcCalcScreen(
+            weight: double.parse(state.params['weight']!),
+            height: double.parse(state.params['height']!),
+            age: int.parse(state.params['age']!),
+            sex: double.parse(state.params['sex']!),
+          ),
+        ),
       ],
     ),
   ],
@@ -37,21 +42,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  /// Constructs a [HomeScreen]
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double _gender = 1.0;
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
+      appBar: AppBar(title: const Text('Form')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
+            TextField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Altura (cm)',
+              ),
+            ),
+            TextField(
+              controller: _weightController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(decimalRegex)
+              ],
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Peso (kg)',
+              ),
+            ),
+            TextField(
+              controller: _ageController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Idade',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Radio(
+                  value: 1.0,
+                  groupValue: _gender,
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = 1.0;
+                    });
+                  },
+                ),
+                const Text('Homem'),
+                Radio(
+                  value: 0.8,
+                  groupValue: _gender,
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = 0.8;
+                    });
+                  },
+                ),
+                const Text('Mulher'),
+              ],
+            ),
             ElevatedButton(
-              onPressed: () => context.go('/$imcCalcRoute}'),
-              child: const Text('Go to the Details screen'),
+              onPressed: () => context.goNamed(imcCalcRoute, params: {
+                "weight": _weightController.text,
+                "height": _heightController.text,
+                "age": _ageController.text,
+                "sex": _gender.toString(),
+              }),
+              child: const Text('Calcular IMC e IGC'),
             ),
           ],
         ),
@@ -63,30 +136,45 @@ class HomeScreen extends StatelessWidget {
 class ImcCalcScreen extends StatelessWidget {
   double weight;
   double height;
+  double sex;
   int age;
-  int sex;
-  ImcCalcScreen(
-      {super.key,
-      required this.weight,
-      required this.height,
-      required this.age,
-      required this.sex});
+  double imc = 0;
+  double igc = 0;
+  ImcCalcScreen({
+    super.key,
+    required this.weight,
+    required this.height,
+    required this.age,
+    required this.sex,
+  });
 
   @override
   Widget build(BuildContext context) {
+    imc = _calculateImc();
+    igc = _calculateIgc();
     return Scaffold(
       appBar: AppBar(title: const Text('Imc calc Screen')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <ElevatedButton>[
+          children: [
+            Text("IMC: ${imc.toStringAsFixed(2)}kg/m2"),
+            Text("IGC: ${igc.toStringAsFixed(2)}"),
             ElevatedButton(
               onPressed: () => context.go('/'),
-              child: const Text('Go back to the Home screen'),
+              child: const Text('VOLTAR'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  double _calculateImc() {
+    return weight / ((height / 100) * (height / 100));
+  }
+
+  double _calculateIgc() {
+    return (1.2 * imc) + (0.23 * age) - (10.8 * sex) - 5.4;
   }
 }
